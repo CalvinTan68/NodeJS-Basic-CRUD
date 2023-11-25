4d8045c66997fd555ca7684d1fc14c715ba9cce4
Calvin Tandika <calvintandikaa68@gmail.com>
2023-11-25 22:57:37 +0700

    Add compression package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import compression from "compression";
 import DataRoute from "./routes/DataRoute.js";
 
 dotenv.config();
@@ -10,6 +11,12 @@ const port = process.env.APP_PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  compression({
+    level: 6,
+    threshold: 0,
+  })
+);
 
 app.use("/data", DataRoute);
 
